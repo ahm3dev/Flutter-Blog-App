619d6e184ab9f60110fccbb6080d674a7ea30a21
r1o <xw6n@hotmail.com>
2021-07-31 21:22:50 +0300

    Added create account screen

---
+++ lib/create_account.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class CreateAccountScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(15),
+        child: SingleChildScrollView(
+                  child: Column(
+            
+            // mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              SizedBox(
+              height: 80,
+            ),
+            Text('Create',
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontSize: 36.0, color: Colors.black, fontFamily: 'SFCompact')),
+              Text('Account',
+              textAlign: TextAlign.left,
+            style: TextStyle(
+              fontSize: 36.0, color: Colors.black, fontFamily: 'SFCompact')),
+            SizedBox(
+              height: 30,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Full Name',
+              )
+              
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Email',
+              )
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Create Password',
+              )
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            TextButton(
+              onPressed: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(
+                //     builder: (context){
+                //       return CreateAccountScreen();
+                //     }
+                //   )
+                // );
+              },
+              child: Text('create account'.toUpperCase()),
+              style: TextButton.styleFrom(
+                primary: Colors.white,
+                backgroundColor: Colors.lightBlue[400],
+                minimumSize: Size(345, 55),
+                textStyle: TextStyle(
+                  fontSize: 16.0,
+                  fontFamily: 'SFCompact',
+                ),
+              ),
+            ),
+          ],),
+        )
+      ),
+    );
+  }
+}
